b45a99f7ab42b65018dda987099857020d8add45
AiR <andytur9@gmail.com>
2024-07-09 13:20:44 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # js
+# Js-beginning
--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log('Hello World')
+
+/*console.log('Hello aaa World')
 console.log('Welcome to my first js world')
+
+1.inventory balance=100
+2.distribution center =30
+
+
+1.inventory remainder 
+
+2. 10
+*/
+
+let inventory=1000;
+let distribution =300;
+let remainder = inventory % distribution;
+console.log(remainder);
+
+let x =22;
+let y =7;
+let m = 4;
+let sum = x % y * m;
+console.log(sum);
+
